6b0624277c655b0962296298ea7bef0e149e0eae
PushPamBoy <dokhiutuan.tom31@gmail.com>
2026-03-22 01:25:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 200); 
+        setTimeout(appendTitle, 150); 
       }
     }
 
